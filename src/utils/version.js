fa4b47eb2df82c18790307c17be0a7aa2add7f54
zgy <834390901@qq.com>
2024-08-07 15:31:34 +0800

    jiaoben

--- src/utils/version.js
+++
@@ -1,7 +0,0 @@
-
-import fs from 'fs';
-import path from 'path';
-
-const v = fs.readFileSync(path.__dirname,'package.json')
-
-console.log(v);
---
+++ version.js
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+import fs from 'fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+try {
+    const filePath = path.join(__dirname, 'package.json')
+    let p = fs.readFileSync(filePath,'utf8')
+    let pData = JSON.parse(p)
+
+    let l = pData.version.split('.')
+    console.log(l);
+    pData.version = [l[0], l[1], Number(l[2]) + 1].join('.')
+
+    const updatePackage = JSON.stringify(pData, null, 2)
+
+    fs.writeFileSync(filePath, updatePackage, 'utf8')
+
+    
+} catch (error) {
+    console.log(error);
+}
